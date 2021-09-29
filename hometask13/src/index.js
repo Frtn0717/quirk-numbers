8628383053c3d5620db646fb5bb9151c8ab9a8c3
Stanislau Turasau <pontty232@gmail.com>
2021-09-30 01:43:32 +0300

    feat: add try/catch option

--- hometask13/src/index.js
+++ hometask13/src/index.js
@@ -5,34 +5,37 @@ const str2 = '1 2 3 4 + × +';
 const str3 = '1 2 3 4 5 2 + × + - /';
 const str4 = '1 2 3 4 6 2 + × * - /';
 
-// const delay = () => new Promise((res, rej) => setTimeout(res, 2000));
 
 const calculate = async (input, stack) => {
   if (typeof input === 'string') {
     input = input.split(' ').map((el) => (isNaN(Number(el)) ? el : Number(el)));
-  }
+  };
 
   console.log('\n Start from: ', input);
 
-  const currChar = input.shift();
-  console.log('input.length = ', input.length);
+  try {
+    const currChar = input.shift();
+    console.log('input.length = ', input.length);
 
-  if (typeof currChar === 'number') {
-    if (!stack) {
-      stack = [currChar];
+    if (typeof currChar === 'number') {
+      if (!stack) {
+        stack = [currChar];
+      } else {
+        stack.push(currChar);
+      };
     } else {
-      stack.push(currChar);
-    }
-  } else {
-    const firstArg = stack.shift();
-    const secondArg = stack.shift();
-    const curResult = await operations[currChar](firstArg, secondArg);
-    console.log('Current result: ', curResult);
-    stack.unshift(curResult);
-  }
+      const firstArg = stack.shift();
+      const secondArg = stack.shift();
+      const curResult = await operations[currChar](firstArg, secondArg);
+      console.log('Current result: ', curResult);
+      stack.unshift(curResult);
+    };
 
-  console.log('Current stack: ', stack)
-  return input.length ? calculate(input, stack) : stack.shift();
+    console.log('Current stack: ', stack);
+    return input.length ? calculate(input, stack) : stack.shift();
+  } catch (error) {
+    console.error('Something went wrong...');
+  };
 };
 
 calculate(str).then(result => console.log('Final result: ', result));
